b2240409bf83e7f63dc866190c4eb32dd2aefc5a
lsd <lsd@olo.kali>
2020-05-18 20:24:28 -0400

    Add spider

--- info/info/spiders/quotes_spider.py
+++ info/info/spiders/quotes_spider.py
@@ -2,19 +2,18 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    name = 'quotes'
+    start_urls = [
+    'http://quotes.toscrape.com/tag/humor/',
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'author': quote.xpath('span/small/text()').get(),
+                'text': quote.css('span.text::text').get(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
